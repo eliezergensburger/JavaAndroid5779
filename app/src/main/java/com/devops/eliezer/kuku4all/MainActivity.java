722acbcf931e91d045fa524071e399ebf039eb74
eliezergensburger <eliezer.jct@gmail.com>
2018-10-18 20:42:41 +0300

    Initial commit

---
+++ app/src/main/java/com/devops/eliezer/kuku4all/MainActivity.java
@@ -0,0 +1,20 @@
+package com.devops.eliezer.kuku4all;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * kuku is tired
+     */
+    public static final String TAG = "stamTag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"on Create Activity");
+    }
+}
